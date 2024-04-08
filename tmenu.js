a06cad914bd43675d5b94fe9fd6f22f56a2bca62
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-04-08 21:38:39 +0100

    fix bug and bug fix

---
+++ tmenu.js
@@ -0,0 +1,9 @@
+const ENTRIES_PATH = "entries_____"; // TODO have this not be a const
+
+chrome.bookmarks.get(ENTRIES_PATH);
+
+document.getElementById("search").oninput.addListener(
+  function() {
+    
+  }
+);
