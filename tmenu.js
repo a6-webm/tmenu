132603dc699f087092cf4fabf2a56559fbbb1c4f
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-04-10 22:57:58 +0100

    get list of entries from hardcoded folder

--- tmenu.js
+++ tmenu.js
@@ -1,9 +1,30 @@
-const ENTRIES_PATH = "entries_____"; // TODO have this not be a const
+// console.debug(chrome.bookmarks.getTree());
+// TODO use promises lmao
+let entries;
+chrome.bookmarks.search("entries")
+  .then(function(entries_node) {
+    flatten_book_tree(entries_node[0])
+      .then(function(new_entries) {
+        entries = new_entries;
+        console.debug(entries);
+      });
+  });
 
-chrome.bookmarks.get(ENTRIES_PATH);
+document.querySelector("#search").oninput = 
+  function(event) {
+    let devices = document.querySelector(".devices");
+    console.debug(this.value); // TODO use this to seach entries
+    // devices.replaceChildren();
+  };
 
-document.getElementById("search").oninput.addListener(
-  function() {
-    
+async function flatten_book_tree(node) {
+  let out = [];
+  if (node.url && (!node.type || node.type == "bookmark" )) {
+    out.push(node);
   }
-);
+  let children = await chrome.bookmarks.getChildren(node.id);
+  for (let child of children) {
+    out = out.concat(await flatten_book_tree(child));
+  }
+  return out;
+}
