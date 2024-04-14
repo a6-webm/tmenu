44d64c984b91e07fa4491f95d387e725d3be6393
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-04-14 22:51:32 +0100

    hocus pocus

---
+++ focus.js
@@ -0,0 +1,1 @@
+location = "tmenu.html";
--- tmenu.js
+++ tmenu.js
@@ -1,9 +1,14 @@
-// TODO make sure that if entries takes time to populate, to not error visibly and not require extra input after entries gets loaded
+// can we open a pop up and then close it
 
 // console.debug(chrome.bookmarks.getTree());
 const fuse_options = {
   keys: ["title"]
 }
+
+let search_elem = document.querySelector("#search");
+let selected_idx = 0;
+
+document.addEventListener("DOMContentLoaded", () => search_elem.focus() );
 
 let entries;
 let fuse;
@@ -12,16 +17,24 @@ chrome.bookmarks.search("entries")
     flatten_book_tree(entries_node[0])
       .then(function(new_entries) {
         entries = new_entries;
-        console.debug(entries);
+        // console.debug(entries);
         fuse = new Fuse(entries, fuse_options);
         reevaluate_options(document.querySelector("#search").value);
       });
   });
 
-document.querySelector("#search").oninput = 
-  function() {
-    reevaluate_options(this.value);
-  };
+window.addEventListener("focus", function (e) {
+  search_elem.focus();
+});
+
+search_elem.addEventListener("focusout", function() {
+  // TODO if options menu opened, don't refocus
+  setTimeout(() => this.focus(), 0);
+});
+
+search_elem.oninput = function() {
+  reevaluate_options(this.value);
+};
 
 function reevaluate_options(query) {
   let res_div = document.querySelector(".results");
@@ -34,11 +47,11 @@ function reevaluate_options(query) {
   let res_elements = [];
   for (let res of search_res) {
     let element = document.createElement("div");
-    console.log(res);
+    // console.log(res);
     element.innerText = res.title;
     res_elements.push(element);
   }
-  console.debug(res_elements);
+  // console.debug(res_elements);
   res_div.replaceChildren(...res_elements);
 }
 
